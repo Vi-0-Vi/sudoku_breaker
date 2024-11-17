ba61077906037ac0d7c18d8ecb30faf3ca9ed398
Ivan <sha_vanya@mail.ru>
2024-11-17 20:35:25 +0300

    create entry list

--- main.py
+++ main.py
@@ -7,13 +7,16 @@ win.title("Sudoku Breaker")
 win.geometry("500x600")
 
 for c in range(9): win.columnconfigure(index=c, weight=1)
-for r in range(9): win.rowconfigure(index=r, weight=1)
+for r in range(10): win.rowconfigure(index=r, weight=1)
+
 def is_valid(string):
     return re.match("^[1-9]?$", string) is not None
+
+entry_list = [[] for _ in range(9)]
 for r in range(9):
     for c in range(9):
         check = (win.register(is_valid), "%P")
-        entry = ttk.Entry(validate="key", validatecommand=check)
-        entry.grid(row=r, column=c, sticky=NSEW)
+        entry_list[r].append(ttk.Entry(justify="center", validate="key", validatecommand=check))
+        entry_list[r][c].grid(row=r, column=c, sticky=NSEW)
 
 win.mainloop()
