32cb972133c4d09ac8178c251b5c92f6c7212693
Ivan <sha_vanya@mail.ru>
2024-11-17 20:14:32 +0300

    pre-filling the form and create valid checking

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-#start working
+from tkinter import *
+from tkinter import ttk
+import re
+
+win = Tk()
+win.title("Sudoku Breaker")
+win.geometry("500x600")
+
+for c in range(9): win.columnconfigure(index=c, weight=1)
+for r in range(9): win.rowconfigure(index=r, weight=1)
+def is_valid(string):
+    return re.match("^[1-9]?$", string) is not None
+for r in range(9):
+    for c in range(9):
+        check = (win.register(is_valid), "%P")
+        entry = ttk.Entry(validate="key", validatecommand=check)
+        entry.grid(row=r, column=c, sticky=NSEW)
+
+win.mainloop()
